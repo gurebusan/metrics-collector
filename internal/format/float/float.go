32a18ff0b3259a34aedcb8c31abbb39d89b3ad53
gurebusan <glebasbylo@ya.ru>
2025-03-02 20:58:04 +0300

    iter3

--- internal/format/float/float.go
+++ internal/format/float/float.go
@@ -6,7 +6,7 @@ import (
 )
 
 // formatFloat форматирует float64, удаляя лишние нули.
-func formatFloat(value float64) string {
+func FormatFloat(value float64) string {
 	// Преобразуем число в строку с максимальной точностью.
 	str := strconv.FormatFloat(value, 'f', -1, 64)
 	// Удаляем лишние нули и точку, если они есть.
