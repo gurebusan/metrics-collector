85602a72fdfc490f7fb2881de37c45d022b6fd57
gurebusan <glebasbylo@ya.ru>
2025-03-18 21:56:00 +0300

    Large refactor and iter7

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/zetcan333/metrics-collector/internal/models"
 	me "github.com/zetcan333/metrics-collector/pkg/myerrors"
 )
 
@@ -13,6 +15,8 @@ import (
 type ServerUseCase interface {
 	UpdateMetric(metricType, metricName, metricValue string) error
 	GetValue(metricType, metricName string) (string, error)
+	UpdateJSON(metric models.Metrics) (models.Metrics, error)
+	GetJSON(metric models.Metrics) (models.Metrics, error)
 	GetAllMetric() (string, error)
 }
 
@@ -25,13 +29,13 @@ func NewServerHandler(suc ServerUseCase) *ServerHandler {
 }
 
 // UpdateHandler обрабатывает запросы на обновление метрик
-func (h *ServerHandler) UpdateHandle(w http.ResponseWriter, r *http.Request) {
+func (h *ServerHandler) UpdateHandler(w http.ResponseWriter, r *http.Request) {
 	metricType := chi.URLParam(r, "type")
 	metricName := chi.URLParam(r, "name")
 	metricValue := chi.URLParam(r, "value")
 
 	if metricName == "" {
-		http.Error(w, "Metric name is required", http.StatusNotFound)
+		http.Error(w, "metric name is required", http.StatusNotFound)
 		return
 	}
 
@@ -69,6 +73,56 @@ func (h *ServerHandler) GetValueHandler(w http.ResponseWriter, r *http.Request) 
 		}
 	}
 	fmt.Fprintf(w, "%s", res)
+}
+
+func (h *ServerHandler) UpdateJSONHandler(w http.ResponseWriter, r *http.Request) {
+	var metric models.Metrics
+	if err := json.NewDecoder(r.Body).Decode(&metric); err != nil {
+		http.Error(w, "invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	updatedMetric, err := h.serverUseCase.UpdateJSON(metric)
+	if err != nil {
+		switch {
+		case errors.Is(err, me.ErrInvalidMetricType),
+			errors.Is(err, me.ErrInvalidGaugeValue),
+			errors.Is(err, me.ErrInvalidCounterValue):
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		default:
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedMetric)
+}
+
+func (h *ServerHandler) GetJSONHandler(w http.ResponseWriter, r *http.Request) {
+	var metric models.Metrics
+	if err := json.NewDecoder(r.Body).Decode(&metric); err != nil {
+		http.Error(w, "invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	result, err := h.serverUseCase.GetJSON(metric)
+	if err != nil {
+		switch {
+		case errors.Is(err, me.ErrMetricNotFound):
+			http.Error(w, err.Error(), http.StatusNotFound)
+		case errors.Is(err, me.ErrInvalidMetricType):
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		default:
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
 }
 
 // GetAllMetricsHandler возвращает все метрики в формате HTML
--- internal/setup/router.go
+++ internal/setup/router.go
@@ -7,8 +7,10 @@ import (
 )
 
 type Handlers interface {
-	UpdateHandle(w http.ResponseWriter, r *http.Request)
+	UpdateHandler(w http.ResponseWriter, r *http.Request)
 	GetValueHandler(w http.ResponseWriter, r *http.Request)
+	UpdateJSONHandler(w http.ResponseWriter, r *http.Request)
+	GetJSONHandler(w http.ResponseWriter, r *http.Request)
 	GetAllMetricsHandler(w http.ResponseWriter, r *http.Request)
 }
 
@@ -24,10 +26,12 @@ func (rr *Routes) SetRoutes(r *chi.Mux) {
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", rr.handlers.GetAllMetricsHandler)
 		r.Route("/update", func(r chi.Router) {
-			r.Post("/{type}/{name}/{value}", rr.handlers.UpdateHandle)
+			r.Post("/{type}/{name}/{value}", rr.handlers.UpdateHandler)
+			r.Post("/", rr.handlers.UpdateJSONHandler)
 		})
 		r.Route("/value", func(r chi.Router) {
 			r.Get("/{type}/{name}", rr.handlers.GetValueHandler)
+			r.Post("/", rr.handlers.GetJSONHandler)
 		})
 	})
 }
