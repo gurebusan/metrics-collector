a07d535734df4d01eee6a560b338a7b8d4741c6e
gurebusan <glebasbylo@ya.ru>
2025-04-05 19:21:23 +0300

    pre iter13

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/stretchr/testify v1.10.0
 require github.com/spf13/pflag v1.0.6
 
 require (
+	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.4 // indirect
--- internal/repo/storage/postgres/postgres.go
+++ internal/repo/storage/postgres/postgres.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
+	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/zetcan333/metrics-collector/internal/models"
 	"github.com/zetcan333/metrics-collector/pkg/myerrors"
@@ -15,12 +18,20 @@ type PgStorage struct {
 	db *pgxpool.Pool
 }
 
+var (
+	delays      = []time.Duration{1 * time.Second, 3 * time.Second, 5 * time.Second}
+	maxAttempts = 3
+)
+
 func NewStorage(ctx context.Context, dataBaseDSN string) (*PgStorage, error) {
 	const op = "internal.repo.storage.postgres.NewStorage"
 
-	pool, err := pgxpool.New(ctx, dataBaseDSN)
+	pool, err := retry(ctx, maxAttempts, delays, func() (*pgxpool.Pool, error) {
+		return pgxpool.New(ctx, dataBaseDSN)
+	})
+
 	if err != nil {
-		return nil, fmt.Errorf("failed to create pool, %w", err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	query := `
@@ -47,27 +58,34 @@ func (p *PgStorage) Ping(ctx context.Context) error {
 func (p *PgStorage) UpdateMetric(ctx context.Context, metric models.Metrics) error {
 	const op = "internal.repo.storage.postgres.UpdateMetric"
 
-	var err error
+	_, err := retry(ctx, maxAttempts, delays, func() (struct{}, error) {
 
-	switch metric.MType {
+		var err error
 
-	case models.Gauge:
-		_, err = p.db.Exec(ctx, `
+		switch metric.MType {
+
+		case models.Gauge:
+			_, err = p.db.Exec(ctx, `
 			INSERT INTO metrics (id, type, value, delta) 
 			VALUES ($1, $2, $3, 0) 
 			ON CONFLICT (id) DO UPDATE 
 			SET value = $3, type = $2
 		`, metric.ID, metric.MType, *metric.Value)
 
-	case models.Counter:
-		_, err = p.db.Exec(ctx, `
+		case models.Counter:
+			_, err = p.db.Exec(ctx, `
 			INSERT INTO metrics (id, type, value, delta)
 			VALUES ($1, $2, 0, $3)
 			ON CONFLICT (id) DO UPDATE
 			SET delta = metrics.delta + $3, type = $2
 		`, metric.ID, metric.MType, *metric.Delta)
-	}
-	return fmt.Errorf("%s: %w", op, err)
+		}
+		if err != nil {
+			return struct{}{}, fmt.Errorf("%s: %w", op, err)
+		}
+		return struct{}{}, nil
+	})
+	return err
 }
 func (p *PgStorage) GetMetric(ctx context.Context, id string) (models.Metrics, error) {
 	const op = "internal.repo.storage.postgres.GetMetric"
@@ -77,108 +95,122 @@ func (p *PgStorage) GetMetric(ctx context.Context, id string) (models.Metrics, e
 		value      float64
 		delta      int64
 	)
-
-	err := p.db.QueryRow(ctx, `SELECT id, type, value, delta FROM metrics WHERE id = $1`, id).
-		Scan(&metric.ID, &metricType, &value, &delta)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return models.Metrics{}, myerrors.ErrMetricNotFound
+	return retry(ctx, maxAttempts, delays, func() (models.Metrics, error) {
+		err := p.db.QueryRow(ctx, `SELECT id, type, value, delta FROM metrics WHERE id = $1`, id).
+			Scan(&metric.ID, &metricType, &value, &delta)
+		if err != nil {
+			if errors.Is(err, pgx.ErrNoRows) {
+				return models.Metrics{}, myerrors.ErrMetricNotFound
+			}
+			return models.Metrics{}, fmt.Errorf("%s: %w", op, err)
 		}
-		return models.Metrics{}, fmt.Errorf("%s: %w", op, err)
-	}
-	metric.MType = metricType
+		metric.MType = metricType
 
-	if metricType == models.Gauge {
-		metric.Value = &value
-	} else if metricType == models.Counter {
-		metric.Delta = &delta
-	}
+		if metricType == models.Gauge {
+			metric.Value = &value
+		} else if metricType == models.Counter {
+			metric.Delta = &delta
+		}
+		return metric, nil
+	})
 
-	return metric, nil
 }
 
 func (p *PgStorage) GetAllGauges(ctx context.Context) (map[string]float64, error) {
 	const op = "internal.repo.storage.postgres.GetAllGauges"
 
-	rows, err := p.db.Query(ctx, `
+	return retry(ctx, maxAttempts, delays, func() (map[string]float64, error) {
+		rows, err := p.db.Query(ctx, `
 		SELECT id, value FROM metrics WHERE type = $1
 	`, models.Gauge)
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-	defer rows.Close()
-
-	gauges := make(map[string]float64)
-	for rows.Next() {
-		var id string
-		var value float64
-		if err := rows.Scan(&id, &value); err != nil {
+		if err != nil {
 			return nil, fmt.Errorf("%s: %w", op, err)
 		}
-		gauges[id] = value
-	}
+		defer rows.Close()
 
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-	return gauges, nil
+		gauges := make(map[string]float64)
+		for rows.Next() {
+			var id string
+			var value float64
+			if err := rows.Scan(&id, &value); err != nil {
+				return nil, fmt.Errorf("%s: %w", op, err)
+			}
+			gauges[id] = value
+		}
+
+		if err := rows.Err(); err != nil {
+			return nil, fmt.Errorf("%s: %w", op, err)
+		}
+		return gauges, nil
+	})
 }
 
 func (p *PgStorage) GetAllCounters(ctx context.Context) (map[string]int64, error) {
 	const op = "internal.repo.storage.postgres.GetAllCounters"
-	rows, err := p.db.Query(ctx, `
+
+	return retry(ctx, maxAttempts, delays, func() (map[string]int64, error) {
+		rows, err := p.db.Query(ctx, `
 	SELECT id, delta FROM metrics WHERE type = $1
 	`, models.Counter)
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-	defer rows.Close()
-	counters := make(map[string]int64)
-	for rows.Next() {
-		var id string
-		var delta int64
-		if err := rows.Scan(&id, &delta); err != nil {
+		if err != nil {
 			return nil, fmt.Errorf("%s: %w", op, err)
 		}
-		counters[id] = delta
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-	return counters, nil
+		defer rows.Close()
+		counters := make(map[string]int64)
+		for rows.Next() {
+			var id string
+			var delta int64
+			if err := rows.Scan(&id, &delta); err != nil {
+				return nil, fmt.Errorf("%s: %w", op, err)
+			}
+			counters[id] = delta
+		}
+		if err := rows.Err(); err != nil {
+			return nil, fmt.Errorf("%s: %w", op, err)
+		}
+		return counters, nil
+	})
 }
 
 func (p *PgStorage) UpdateMetricsWithBatch(ctx context.Context, metrics []models.Metrics) error {
 	const op = "internal.repo.storage.postgres.UpdateMetricsWithBatch"
-	tx, err := p.db.Begin(ctx)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
-	defer tx.Rollback(ctx)
-	for _, metric := range metrics {
-		var err error
-		switch metric.MType {
-		case models.Gauge:
-			_, err = tx.Exec(ctx, `
+
+	_, err := retry(ctx, maxAttempts, delays, func() (struct{}, error) {
+
+		tx, err := p.db.Begin(ctx)
+		if err != nil {
+			return struct{}{}, fmt.Errorf("%s: %w", op, err)
+		}
+		defer tx.Rollback(ctx)
+		for _, metric := range metrics {
+			var err error
+			switch metric.MType {
+			case models.Gauge:
+				_, err = tx.Exec(ctx, `
 				INSERT INTO metrics (id, type, value, delta) 
 				VALUES ($1, $2, $3, 0) 
 				ON CONFLICT (id) DO UPDATE 
 				SET value = $3, type = $2
 			`, metric.ID, metric.MType, *metric.Value)
-		case models.Counter:
-			_, err = tx.Exec(ctx, `
+			case models.Counter:
+				_, err = tx.Exec(ctx, `
 				INSERT INTO metrics (id, type, value, delta)
 				VALUES ($1, $2, 0, $3)
 				ON CONFLICT (id) DO UPDATE
 				SET delta = metrics.delta + $3, type = $2
 			`, metric.ID, metric.MType, *metric.Delta)
+			}
+			if err != nil {
+				tx.Rollback(ctx)
+				return struct{}{}, fmt.Errorf("%s: %w", op, err)
+			}
 		}
-		if err != nil {
-			tx.Rollback(ctx)
-			return fmt.Errorf("%s: %w", op, err)
+		if err := tx.Commit(ctx); err != nil {
+			return struct{}{}, fmt.Errorf("%s: %w", op, err)
 		}
-	}
-	return tx.Commit(ctx)
+		return struct{}{}, nil
+	})
+	return err
 }
 
 // mock SaveBkpToFile and LoadBkpFromFile
@@ -187,4 +219,38 @@ func (p *PgStorage) SaveBkpToFile(path string) error {
 }
 func (p *PgStorage) LoadBkpFromFile(path string) error {
 	return nil
+}
+
+func retry[T any](ctx context.Context, maxAttempts int, delays []time.Duration, fn func() (T, error)) (T, error) {
+	var zero T
+
+	for attempt := 0; attempt < maxAttempts; attempt++ {
+		result, err := fn()
+
+		switch {
+
+		case err == nil:
+			return result, nil
+
+		case !isRetriableError(err):
+			return zero, err
+		}
+
+		select {
+		case <-ctx.Done():
+			return zero, ctx.Err()
+		case <-time.After(delays[attempt]):
+		}
+	}
+	return zero, fmt.Errorf("max attempts reached")
+}
+
+func isRetriableError(err error) bool {
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		return pgErr.Code == pgerrcode.ConnectionException ||
+			pgErr.Code == pgerrcode.ConnectionDoesNotExist ||
+			pgErr.Code == pgerrcode.ConnectionFailure
+	}
+	return false
 }
