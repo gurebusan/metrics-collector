538bad7fd69059da2ddc6ef1ca3348e9f5b29024
gurebusan <glebasbylo@ya.ru>
2025-04-25 01:24:00 +0300

    ...

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -218,9 +218,9 @@ func (a *Agent) SendMetricsBatch() error {
 		req.Header.Set("HashSHA256", hash)
 	}
 	req.Header.Set("Content-Type", "application/json")
-	fmt.Println("📦 AGENT BODY:", string(body))
-	fmt.Printf("📦 AGENT RAW: %x\n", body)
-	fmt.Println("🔐 AGENT HASH:", createHash(body, key))
+	fmt.Println(" AGENT BODY:", string(body))
+	fmt.Printf(" AGENT RAW: %x\n", body)
+	fmt.Println(" AGENT HASH:", createHash(body, a.flags.Key))
 	return retry(maxAttempts, delays, func() error {
 
 		resp, err := a.client.Do(req)
